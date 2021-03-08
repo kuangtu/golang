bdd93ba2a5f48bd51d07d00087bee4b59223afca
kuangtu <kuangtuweb@gmail.com>
2021-03-08 23:15:42 +0800

    go cached

---
+++ pkg_usage/README.md
@@ -0,0 +1,32 @@
+# 一、常用包的使用
+
+
+
+## 1、命令行处理flag包
+
+Golang 的标准库提供了 flag 包来处理命令行参数。
+
+
+
+# 二、其他包的使用
+
+
+
+## 2.1、go-cach使用
+
+go-cache是单机版本的内存key-value缓存库。任何对象都可以被存储，给定一个有效存续时间或者永久，cache可以被多个协程安全使用。
+
+https://github.com/patrickmn/go-cache
+
+
+
+## 2.2 、基本使用
+
+参照 [示例程序](go-cache/cache-test.go):
+
+```go
+
+```
+
+
+
---
+++ pkg_usage/go-cache/src/cache-test.go
@@ -0,0 +1,20 @@
+package main
+
+
+import (
+    "fmt"
+    "github.com/patrickmn/go-cache"
+    "time"
+)
+
+func main() {
+    //创建一个缓存
+    c := cache.New(5*time.Minute, 10*time.Minute)
+    c.Set("foo", "bar", cache.DefaultExpiration)
+    
+    foo, found := c.Get("foo")
+    if found {
+        fmt.Println(foo)
+    }
+
+}
