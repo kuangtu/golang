8dffc35284d16a15fda044edd9267ecb2286a444
kuangtu <kuangtuweb@gmail.com>
2021-05-16 23:08:46 +0800

    binary encoding

--- chap13/bin_io_encoding.go
+++ chap13/bin_io_encoding.go
@@ -12,12 +12,33 @@ type packet struct {
     Locid  uint16
     Tstamp uint32
     Temp   int16
-    cs string
 }
+
+type MsgHeader struct {
+    MsgType [4]byte
+    SendingTtime uint64
+    MsgSeq uint64
+    BodyLength uint32
+}
+
+type MsgTail struct {
+    CheckSum uint32
+}
+
+//登录消息
+type LoginMsg struct {
+    Header MsgHeader
+    SenderCompID    [32]byte
+    TargetCompID    [32]byte
+    HeartBtInt      uint16
+    AppVerID        [8]byte
+    Tail MsgTail
+}
+
 
 func main() {
     dataIn := packet {
-        Sensid: 1, Locid: 1233, Tstamp: 123452123, Temp: 12,cs: "test",
+        Sensid: 1, Locid: 1233, Tstamp: 123452123, Temp: 12,
     }
     
     buf := new(bytes.Buffer)
@@ -31,5 +52,38 @@ func main() {
     
     //Buffer长度
     fmt.Printf("%d\n", buf.Len())
+    
+    var dataOut packet
+    if err := binary.Read(buf, binary.BigEndian, &dataOut); err != nil {
+        fmt.Println("failed to Read:", err)
+        return
+    }
+    
+    fmt.Printf("the Sensid is:%d\n", dataOut.Sensid)
+
+    
+    //消息头部写入
+    buf.Reset()
+    
+    header := MsgHeader {
+        MsgType: [...]byte{0,0,0,0}, SendingTtime: 1, MsgSeq: 1, BodyLength: 22,
+    }
+    
+    tailmsg := MsgTail {
+        CheckSum: 0x11,
+    }
+    
+    loginmsg := LoginMsg {
+        Header: header, Tail: tailmsg,
+    }
+        
+    err = binary.Write(buf, binary.BigEndian, loginmsg)
+    
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    
+    
     
 }
