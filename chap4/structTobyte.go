fc7f6f2305845a519407facbbf9bbefc03f08883
kuangtu <kuangtuweb@gmail.com>
2021-06-21 23:29:26 +0800

    struct

--- chap4/structTobyte.go
+++ chap4/structTobyte.go
@@ -4,28 +4,91 @@ import (
 	"fmt"
 )
 
-type LoginMsg struct {
+type CheckSum interface {
+	CalCheckSum() uint32
+}
+
+func GetPktChkSum(chksum CheckSum) uint32 {
+	return chksum.CalCheckSum()
+}
+
+const (
+	MsgType_LEN      = 4
+	SenderCompID_LEN = 32
+	TargetCompID_LEN = 32
+	AppVerID_LEN     = 8
+	//消息体长度
+	MsgHeader_LEN     = 24
+	LoginMsg_BODY_LEN = 15
+)
+
+var (
+	Pkt_MsgSeq uint64 = 1
+)
+
+type MsgHeader struct {
 	MsgType      [4]byte
-	Time         uint64
-	SenderCompID [32]byte
-	TargetCompID [32]byte
+	SendingTtime uint64
+	MsgSeq       uint64
+	BodyLength   uint32
+}
+
+type MsgTail struct {
+	CheckSum uint32
+}
+
+type LoginMsg struct {
+	Header       MsgHeader
+	SenderCompID [SenderCompID_LEN]byte
+	TargetCompID [TargetCompID_LEN]byte
 	HeartBtInt   uint16
-	AppVerID     [8]byte
+	AppVerID     [AppVerID_LEN]byte
 	CheckSum     uint32
+	Tail         MsgTail
 }
 
 func main() {
 	fmt.Println("test")
-	b := []byte("M001")
-	loginMsg := LoginMsg{Time: 111, HeartBtInt: 111}
-	fmt.Println(loginMsg.Time)
-	//通过数组赋值
-	loginMsg.MsgType = [4]byte{'M', '0', '0', '1'}
-	fmt.Println(loginMsg.MsgType)
+	var mystr []byte
+	mystr = []byte("M001")
+	loginMsg := LoginMsg{}
 
-	//通过切片赋值
-	for i, x := range b {
-		loginMsg.MsgType[i] = x
+	//填充发送ID
+	var index int
+	for index = 0; index < SenderCompID_LEN; index++ {
+		loginMsg.SenderCompID[index] = ' '
+	}
+	mystr = []byte("csisender")
+	for i, x := range mystr {
+		loginMsg.SenderCompID[i] = x
 	}
 
+	//填充目标ID
+	for index = 0; index < TargetCompID_LEN; index++ {
+		loginMsg.TargetCompID[index] = ' '
+	}
+	mystr = []byte("csitarget")
+	for i, x := range mystr {
+		loginMsg.TargetCompID[i] = x
+	}
+
+	//填充APPVerID
+	for index = 0; index < AppVerID_LEN; index++ {
+		loginMsg.AppVerID[index] = ' '
+	}
+	mystr = []byte("1.00")
+	for i, x := range mystr {
+		loginMsg.AppVerID[i] = x
+	}
+
+	//填充消息头部
+	loginMsg.Header.MsgType = [4]byte{'M', '0', '0', '1'}
+	loginMsg.Header.SendingTtime = 1234
+	loginMsg.Header.MsgSeq = Pkt_MsgSeq
+	loginMsg.Header.BodyLength = LoginMsg_BODY_LEN
+
+	fmt.Println("the body len is:", loginMsg.Header.BodyLength)
+	//计算消息的CHECKSUM
+
+	//将结构体放入到byteBuferr中
 }
