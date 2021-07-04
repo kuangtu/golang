8d76ced0e56610a0ee9013a7b7319bd004673f9d
kuangtu <kuangtuweb@gmail.com>
2021-07-04 21:39:10 +0800

    array test

--- chap14/thdechoserver.go
+++ chap14/thdechoserver.go
@@ -1,55 +1,59 @@
 package main
 
-import(
-    "net"
-    "os"
-    "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
 )
 
 func main() {
 
-    service := ":1200"
+	service := "192.168.0.106:20001"
 
-    //解析TCP地址及端口
-    tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
-    checkError(err)
-    
-    //监听端口
-    listener, err := net.ListenTCP("tcp", tcpAddr)
-    checkError(err)
-    
-    for {
-        conn, err := listener.Accept()
-        
-        if err != nil {
-            conntinue
-        }
-        
-        go handleClient(conn)
-    }
-    
+	//解析TCP地址及端口
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	checkError(err)
+
+	//监听端口
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	for {
+		conn, err := listener.Accept()
+
+		if err != nil {
+			continue
+		}
+
+		go handleClient(conn)
+	}
+
 }
 
 func handleClient(conn net.Conn) {
-    defer conn.Close()
-    
-    var buf [512]byte
-    for {
-        n, err := conn.Read(buf[0:])
-        if err != nil {
-            return
-        }
-        
-        //write data，收到了
-        _, err2 := conn.Write(buf[0:n])
-        if err2 != nil {
-            return 
-        }
-    }
+	defer conn.Close()
+	fmt.Println("start handler client.")
+	var buf [512]byte
+	for {
+		n, err := conn.Read(buf[0:])
+		if err != nil {
+			fmt.Println("read error:", n)
+			return
+		}
+		fmt.Println("read number is:", n)
+
+		//write data，收到了
+		n, err2 := conn.Write(buf[0:n])
+		if err2 != nil {
+			return
+		}
+		fmt.Println("write number is:", n)
+	}
 }
 
 func checkError(err error) {
-    if err != nil {
-        fmt.Println(os.Stderr, "Fatal error: %s", err.Error())
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Println(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
--- chap4/array.go
+++ chap4/array.go
@@ -3,11 +3,18 @@ package main
 import "fmt"
 
 func main() {
-    var a [3]int
-    fmt.Println(a[0])
-    fmt.Println(len(a))
-    
-    for i, v := range a{
-        fmt.Printf("%d %d\n", i, v)
-    }
+	var a [3]int
+	fmt.Println(a[0])
+	fmt.Println(len(a))
+
+	for i, v := range a {
+		fmt.Printf("%d %d\n", i, v)
+	}
+
+	//基本类型，数组比较:
+	b1 := [4]byte{'a', 'b', 'c'}
+	b2 := [4]byte{'a', 'b', 'c'}
+	if b1 == b2 {
+		fmt.Println("the two array equal")
+	}
 }
