eb4cfd1c7f2fc24d29899c7a74ab39494d0331a3
kuangtu <kuangtuweb@gmail.com>
2021-10-30 23:14:39 +0800

    chap2

---
+++ chap2/const.go
@@ -0,0 +1,17 @@
+// Boiling prints the boiling point of water.
+package main
+
+import "fmt"
+
+const (
+    X float32 = 3.14
+    Y
+    Z
+    A = "test"
+    B
+)
+
+func main() {
+    fmt.Println(Z)
+    fmt.Println(B)
+}
---
+++ chap2/identify.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var 大学="lzu"
+    fmt.Println(大学)
+}
+   
